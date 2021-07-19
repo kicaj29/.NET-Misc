e085c3ef8336994f17f2ad13185a3883bb99a135
kicaj29 <kicaj29@wp.pl>
2021-07-19 11:44:47 +0200

    added types from System.Net.Http

---
+++ MultiTargeting/MultiTargeting/Services/MyService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace MultiTargeting.Services
+{
+    public class MyService
+    {
+        public async void CallSomeEndpointAsync()
+        {
+            HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Get, new Uri("not set", UriKind.Absolute));
+            HttpClient client = new HttpClient();
+            System.Net.Http.HttpResponseMessage response = await client.SendAsync(request);
+        }
+    }
+}
