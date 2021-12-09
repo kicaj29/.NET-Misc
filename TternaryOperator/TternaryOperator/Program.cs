e5798495f8ea6f76c8c1f8e7c20a9e16bd35ad53
Jacek Kowalski <Jacek.Kowalski@hyland.com>
2021-12-09 13:58:57 +0100

    TternaryOperator

---
+++ TternaryOperator/TternaryOperator/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace TternaryOperator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int? a = null;
+            int b = a ?? -1;
+            Console.WriteLine(b);  // output: -1
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
