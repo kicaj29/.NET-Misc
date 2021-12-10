48aaed1ac77d2f452144d04e12abfdf64395d1f6
Jacek Kowalski <Jacek.Kowalski@hyland.com>
2021-12-10 12:43:59 +0100

    UsingAndDispose

---
+++ UsingAndDispose/UsingAndDispose/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace UsingAndDispose
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceRegistry reg = new ServiceRegistry();
+            // https://docs.microsoft.com/en-us/dotnet/csharp/language-reference/keywords/using-statement
+            // "The using statement ensures that Dispose (or DisposeAsync) is called even if an exception occurs within the using block."
+
+
+            using (CustomerService custSvc = reg.GetCusomterService())
+            {
+                // Dispose method is not called when debugging in VS
+                // but is called when we run the console app without VS!!!!
+                // EXPLANATION: https://stackoverflow.com/questions/49106975/why-using-does-not-call-dispose-method-in-debugger
+                // https://docs.microsoft.com/en-us/visualstudio/debugger/managing-exceptions-with-the-debugger?view=vs-2022
+                // it does not help here: https://docs.microsoft.com/en-us/visualstudio/debugger/managing-exceptions-with-the-debugger?view=vs-2022#BKMK_UserUnhandled
+                // throw new Exception("Fail!!!");
+            }
+
+
+            MainAsync().GetAwaiter().GetResult();
+
+            Console.WriteLine("END");
+            Console.ReadKey();
+        }
+
+        private static async Task MainAsync()
+        {
+            ServiceRegistry reg = new ServiceRegistry();
+            using (CustomerService custSvc = await reg.GetCusomterServiceAsync())
+            {
+
+            }
+        }
+    }
+}
---
+++ UsingAndDispose/UsingAndDispose/ServiceRegistry.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UsingAndDispose
+{
+    public class ServiceRegistry
+    {
+        public CustomerService GetCusomterService()
+        {
+            return new CustomerService();
+            // return null;
+        }
+
+        public Task<CustomerService> GetCusomterServiceAsync()
+        {
+            return Task.FromResult(new CustomerService());
+        }
+    }
+}
