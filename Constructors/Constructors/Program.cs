f9257e3c4e3baf7a947e857c81140b789458f713
Jacek Kowalski <Jacek.Kowalski@hyland.com>
2021-12-10 12:00:10 +0100

    Constructors

---
+++ Constructors/Constructors/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Constructors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var col = new SettingsCollection();
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Constructors/Constructors/SettingsCollection.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructors
+{
+    public class SettingsCollection
+    {
+        public SettingsCollection()
+        {
+            Settings = Array.Empty<Setting>();
+        }
+
+        private SettingsCollection(bool isReadonly):this()
+        {
+            this._isReadOnly = isReadonly;
+        }
+
+        static SettingsCollection()
+        {
+            Empty = new SettingsCollection(true);
+        }
+
+        private readonly bool _isReadOnly;
+        public static SettingsCollection Empty;
+        private Setting[] _settings { get; set; }
+        public Setting[] Settings
+        {
+            get { return _settings; }
+            set
+            {
+                if (_isReadOnly)
+                    throw new InvalidOperationException("Cannot modify readonly settings.");
+                _settings = value;
+            }
+        }
+    }
+}
