ca6deeac1a3a2dd69fbdebcb56f909ec4186cae9
Jacek Kowalski <Jacek.Kowalski@hyland.com>
2022-02-02 12:32:39 +0100

    Enums

---
+++ Enums/Enums/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Enums
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // try parse executes trim operation before parsing!
+            if (Enum.TryParse("  ExpORted  ", ignoreCase: true, out MyEnum result))
+            {
+                Console.WriteLine(result);
+            }
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
