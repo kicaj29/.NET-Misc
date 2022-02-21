a6d5e4fb3c6b7099cedb93ca950df06a997aaacc
Jacek Kowalski <Jacek.Kowalski@hyland.com>
2022-02-21 16:12:56 +0100

    hash function

---
+++ HashFunctions/HashFunctions/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace HashFunctions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+            for(int index =0; index < 200; index++)
+            {
+                HashCode.Combine("AAAAAAA", "BBBBBBB", "CCCCCCCC", "DDDDDDD", "EEEEEEE", "FFFFFFF");
+            }
+            sw.Stop();
+            Console.WriteLine(sw.Elapsed);
+            Console.ReadKey();
+
+        }
+    }
+}
