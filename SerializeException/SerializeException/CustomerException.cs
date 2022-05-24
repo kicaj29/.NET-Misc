4837da84fae3d4ab2261803e8a79240a5c427c8c
Jacek Kowalski <Jacek.Kowalski@hyland.com>
2022-05-24 10:00:29 +0200

    SerializeException

---
+++ SerializeException/SerializeException/CustomerException.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace SerializeException
+{
+    public class CustomerException : Exception
+    {
+
+        public CustomerException(string firstName, string secondName) : base()
+        {
+            this.FirstName = firstName;
+            this.SecondName = secondName;
+        }
+
+        public string FirstName { get; private set; }
+        public string SecondName { get; private set; }
+
+
+        public override void GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            base.GetObjectData(info, context);
+            info.AddValue("firstName", FirstName);
+        }
+    }
+}
---
+++ SerializeException/SerializeException/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace SerializeException
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            try
+            {
+                throw new CustomerException("Jacek", "Kowalski");
+            }
+            catch(Exception ex)
+            {
+                var data = ex.Data;
+                foreach(var item in data)
+                {
+
+
+                }
+
+            }
+        }
+    }
+}
