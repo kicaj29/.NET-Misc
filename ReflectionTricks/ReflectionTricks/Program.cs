5462f384665933f1038d9348e282bd3e98c22d02
Jacek Kowalski <Jacek.Kowalski@hyland.com>
2022-11-29 11:45:28 +0100

    ReflectionTricks

---
+++ ReflectionTricks/ReflectionTricks/Program.cs
@@ -0,0 +1,13 @@
+﻿using ReflectionTricks;
+using System.Reflection;
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+MethodInfo mi = typeof(Example).GetMethod(nameof(Example.Add));
+
+MethodBody mb = mi.GetMethodBody();
+byte[] bodyBytes = mb.GetILAsByteArray();
+
+
+Console.ReadKey();
