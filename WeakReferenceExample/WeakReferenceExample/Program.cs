e306cec292499adca7ef5cde18fdf737217cc7f0
Jacek Kowalski <Jacek.Kowalski@hyland.com>
2023-03-02 14:19:02 +0100

    WeakReferenceExample

---
+++ WeakReferenceExample/WeakReferenceExample/Program.cs
@@ -0,0 +1,27 @@
+﻿// https://learn.microsoft.com/en-us/dotnet/api/system.weakreference?view=net-7.0
+
+// "A weak reference allows the garbage collector to collect an object while still allowing an application to access the object."
+
+// Create the cache.
+using WeakReferenceExample;
+
+int cacheSize = 50;
+Random r = new Random();
+Cache c = new Cache(cacheSize);
+
+string DataName = "";
+GC.Collect(0);
+
+// Randomly access objects in the cache.
+for (int i = 0; i < c.Count; i++)
+{
+    int index = r.Next(c.Count);
+
+    // Access the object by getting a property value.
+    DataName = c[index].Name;
+}
+// Show results.
+double regenPercent = c.RegenerationCount / (double)c.Count;
+Console.WriteLine($"Cache size: {c.Count}, Regenerated: {regenPercent:P0}");
+
+Console.WriteLine("-----------END-------------------");
