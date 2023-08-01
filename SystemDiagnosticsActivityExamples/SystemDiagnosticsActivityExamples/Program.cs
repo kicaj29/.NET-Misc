10b91ef154a0619fb4dac94c5f32fb1aacb4ab92
Jacek Kowalski <Jacek.Kowalski@hyland.com>
2023-08-01 11:41:05 +0200

    SystemDiagnosticsActivityExamples

---
+++ SystemDiagnosticsActivityExamples/SystemDiagnosticsActivityExamples/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Diagnostics;
+
+Console.WriteLine("Hello, World!");
+
+// https://www.w3.org/TR/trace-context/#traceparent-header
+
+// In this way we can create activity with selected traceId.
+string traceId = "5bc8be621de4fccb9ac7df8dd6583333";
+ActivityTraceId activityTraceId = ActivityTraceId.CreateFromString(traceId);
+
+var activity = new Activity("MyTestActivity");
+activity.SetIdFormat(ActivityIdFormat.W3C);
+
+Console.WriteLine(activity.Id);
+
+ActivitySpanId createdActivitySpanId = ActivitySpanId.CreateRandom();
+
+activity.SetParentId(activityTraceId, createdActivitySpanId, ActivityTraceFlags.Recorded);
+//activity.SetParentId("5bc8be621de4fccb9ac7df8dd6583333");
+
+activity.Start();
+
+Console.WriteLine(activity.Id);
+Console.WriteLine(Activity.Current?.Id);
+
+Console.ReadKey();
+
