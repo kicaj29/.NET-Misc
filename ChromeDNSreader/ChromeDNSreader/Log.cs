2f1ed3e337122a371490beb81ac6c261f8b6899d
Jacek Kowalski <kicaj29@wp.pl>
2024-12-26 16:31:55 +0100

    chrome dns reader

---
+++ ChromeDNSreader/ChromeDNSreader/Log.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChromeDNSreader
+{
+    public class Log
+    {
+        public List<Entry> Entries { get; set; } = [];
+    }
+}
---
+++ ChromeDNSreader/ChromeDNSreader/Program.cs
@@ -0,0 +1,36 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ChromeDNSreader;
+using System.Text.Json;
+
+Console.WriteLine("Starting...");
+
+string jsonString = File.ReadAllText("poki.com.har.json");
+
+RootModel rootModel = JsonSerializer.Deserialize<RootModel>(jsonString,
+    new JsonSerializerOptions()
+    {
+        PropertyNameCaseInsensitive = true
+    }) ?? throw new InvalidOperationException();
+
+List<string> urls = rootModel.Log.Entries.Select(entry => entry.Request.Url).ToList();
+
+HashSet<string> domains = [];
+
+foreach (string url in urls)
+{
+    int startIndex = url.IndexOf("://") + 3;
+    string tmp = url.Substring(startIndex);
+    int endIndex = tmp.IndexOf('/');
+    tmp = tmp.Substring(0, endIndex);
+    if (!domains.Contains(tmp))
+    {
+        domains.Add(tmp);
+    }
+}
+
+foreach (string domain in domains)
+{
+    Console.WriteLine(domain);
+}
+
+Console.WriteLine("Finished");
