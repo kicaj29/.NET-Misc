dd9c5f72fba70cc00634b17b15cbe66c1df9270c
Jacek Kowalski <Jacek.Kowalski@hyland.com>
2025-09-24 13:23:47 +0200

    ArrayBenchmark

---
+++ MemoryOptimizations/MemoryOptimizations/ArrayBenchmark.cs
@@ -0,0 +1,45 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Jobs;
+using System;
+using System.Buffers;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MemoryOptimizations
+{
+    [MemoryDiagnoser]
+    [SimpleJob(RuntimeMoniker.Net80)]
+    public class ArrayBenchmark
+    {
+        [Benchmark]
+        public void NormalArray()
+        {
+            for (int a = 0; a < 10; a++)
+            {
+                var intArray = new int[1000];
+                for (int i = 0; i < 1000; i++)
+                {
+                    intArray[i] = i;
+                }
+            }
+        }
+
+        [Benchmark]
+        public void ArrayPool()
+        {
+            var shared = ArrayPool<int>.Shared;
+            for (int a = 0; a < 10; a++)
+            {
+                var intArray = shared.Rent(1000);
+                for (int i = 0; i < 1000; i++)
+                {
+                    intArray[i] = i;
+                }
+                shared.Return(intArray);
+            }
+        }
+    }
+}
---
+++ MemoryOptimizations/MemoryOptimizations/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+using BenchmarkDotNet.Running;
+using MemoryOptimizations;
+
+Console.WriteLine("Hello, World!");
+
+BenchmarkRunner.Run<ArrayBenchmark>();
